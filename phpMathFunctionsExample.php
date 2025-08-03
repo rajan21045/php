5efecd2073f9856cca682b4c96a04c288c06521a
Rajan Poudel <raajan.works@gmail.com>
2025-08-03 16:50:09 +0545

    Add files via upload

---
+++ phpMathFunctionsExample.php
@@ -0,0 +1,15 @@
+<?php
+echo (pi());
+
+echo (sqrt(16));
+echo (pow(2, 3));
+echo (abs(-5));
+echo (max(10, 20, 30));
+echo (min(10, 20, 30));
+echo (round(3.6));
+echo (floor(3.6));
+echo (ceil(3.6));
+echo (rand(1, 100));
+
+
+?>
